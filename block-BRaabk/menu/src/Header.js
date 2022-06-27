6d3508725907847fa777cb071cb35a84439726a0
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-27 13:01:03 +0530

    Assignment: Food Menu App

---
+++ block-BRaabk/menu/src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="container">
+      <header>
+        <p className="people">Our Menu</p>
+      </header>
+      <hr />
+    </div>
+  );
+}
+export default Header;
